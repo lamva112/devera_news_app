a84af624c046c70ff2848ddfbe84434a6cf91c13
lambui <lambui@lecle.co.kr>
2023-09-10 22:43:49 +0700

    [add] - home page and news api

---
+++ lib/page/home/home_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+class HomePage extends StatefulWidget {
+  static const routeName = "/home-page";
+
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+  final bottomBarIcon = [
+    IconlyLight.home,
+    IconlyLight.discovery,
+    Icons.sunny_snowing,
+    Icons.settings,
+  ];
+
+  List<String> bottomBarTitle = ["Home", "Trending", "Weather", "Setting"];
+
+  List<Widget> tabsScreen = [];
+
+  @override
+  void didChangeDependencies() {
+    tabsScreen.addAll([
+      Container(),
+      Container(),
+      Container(),
+      Container(),
+    ]);
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: tabsScreen,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          if (index != currentIndex) {
+            setState(() {
+              currentIndex = index;
+            });
+          }
+        },
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.lightBlue,
+        selectedLabelStyle: Theme.of(context).textTheme.caption?.copyWith(
+              fontWeight: FontWeight.w500,
+              color: Colors.lightBlue,
+            ),
+        unselectedLabelStyle: Theme.of(context).textTheme.caption?.copyWith(
+              fontWeight: FontWeight.w500,
+            ),
+        items: List.generate(
+          tabsScreen.length,
+          (index) => BottomNavigationBarItem(
+            icon: Icon(bottomBarIcon[index]),
+            label: bottomBarTitle[index],
+            activeIcon: Icon(
+              bottomBarIcon[index],
+              color: Colors.lightBlue,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
